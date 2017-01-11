6846c6c6c4fb2e23280ece3b388f35f5a53d9b80
Noro Korny <korny@softival.com>
2017-01-11 22:24:51 +0100

    Covariance and Contravariance

---
+++ Playground/src/Playground.CovarianceAndContravariance/IFeature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.CovarianceAndContravariance
+{
+    public interface IVariantCollection<T> // where TC: T
+    {
+        IEnumerable<T> List { get; set; }
+    }
+
+    public class VariantCollection<T>: IVariantCollection<T>
+    {
+        public IEnumerable<T> List { get; set; }
+    }
+
+    public interface IFeature
+    {
+        string Data { get; set; }
+    }
+
+    public class Feature : IFeature
+    {
+        public string Data { get; set; }
+    }
+
+
+   
+}
---
+++ Playground/src/Playground.CovarianceAndContravariance/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Playground.CovarianceAndContravariance
+{
+    public class Main
+    {
+        public IVariantCollection<IFeature> Test()
+        {
+            var list = new VariantCollection<IFeature>();
+
+            list.List = new List<Feature>();
+
+            return list;
+        }
+
+    }
+}
